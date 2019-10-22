8c9942a58c839295bbf05cb4f1ce1429ca982c15
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-22 10:50:05 +0900

    feat(coding): event handler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickResult: "",
+      changeResult: "",
+      inputResult: "",
+      keyupResult: ""
+    };
+  }
+
+  onClick() {
+    this.setState({
+      clickResult: "clicked"
+    });
+  }
+
+  onChange(e) {
+    this.setState({
+      changeResult: "change"
+    });
+  }
+
+  onInput(e) {
+    this.setState({
+      inputResult: "input.."
+    });
+  }
+
+  onKeyUp(e) {
+    this.setState({
+      keyupResult: "up"
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClick.bind(this)}>Click!</button>{this.state.clickResult}<br />
+        <input onChange={this.onChange.bind(this)} placeholder="change" />{this.state.changeResult}<br />
+        <input onInput={this.onInput.bind(this)} placeholder="input" />{this.state.inputResult}<br />
+        <input onKeyUp={this.onKeyUp.bind(this)} placeholder="keyup" />{this.state.keyupResult}
+      </div>
+    )
+  }
+
 }
 
 export default App;
